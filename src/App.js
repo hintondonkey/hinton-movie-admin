862d05600f69f84cb634a2aae4682bbd6d8254f8
LAPTOP-05U5BG7U\Admin <kimhexi1998@gmail.com>
2023-05-05 00:30:24 +0700

    impl.implement new form create movie

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './Login'
-import Listmovie from './components/ListMovie'
-import AddMovieTicket from './components/AddMovieTicket'
-import { ToastContainer } from 'react-toastify'
-import EditMovieTicket from './components/EditMovieTicket'
-import LoginPage from './view/login_page'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Listmovie from './components/ListMovie';
+import AddMovieTicket from './components/AddMovieTicket';
+import { ToastContainer } from 'react-toastify';
+import EditMovieTicket from './components/EditMovieTicket';
+import LoginPage from './view/login_page';
+import AddMoviePage from './view/add_movie_page';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/listmovie">
-            <Route path=":id" element={<EditMovieTicket />} />
-            <Route index element={<Listmovie />} />
-          </Route>
-          <Route path="/addmovie" element={<AddMovieTicket />} />
-        </Routes>
-      </BrowserRouter>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      {/* Same as */}
-      <ToastContainer />
-    </div>
-  )
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/listmovie">
+                        <Route path=":id" element={<EditMovieTicket />} />
+                        <Route index element={<Listmovie />} />
+                    </Route>
+                    <Route path="/addmovie" element={<AddMoviePage />} />
+                </Routes>
+            </BrowserRouter>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            {/* Same as */}
+            <ToastContainer />
+        </div>
+    );
 }
 
-export default App
+export default App;
